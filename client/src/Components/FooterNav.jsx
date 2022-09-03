b46f675fc4c2ca0cb556762a1e9a7a51d91c57cb
sanketg <sanketgawande.gcoey@gmail.com>
2022-09-03 20:34:48 +0530

    footer ui issue

--- client/src/Components/FooterNav.jsx
+++ client/src/Components/FooterNav.jsx
@@ -6,8 +6,8 @@ const FooterNav = () => {
     return (
         <>
             {/* <div className='h-[100px] md:h-[80px]'></div> */}
-            <div className='bg-white shadow-md fixed bottom-0 md:bottom-6 md:-right-44 hover:-right-2 transition-all text-sm font-semibold border  w-full  md:w-48 justify-center items-center flex md:flex-col text-center h-[50px] md:h-auto'>
-                <span className='inline-block left-[-24px] absolute h-12 w-12'>
+            <div className='bg-white shadow-md fixed bottom-0 md:bottom-6 md:-right-44 md:hover:-right-2 transition-all text-sm font-semibold border  w-full  md:w-48 justify-center items-center flex md:flex-col text-center h-[50px] md:h-auto'>
+                <span className='md:first-letter:inline-block left-[-24px] absolute h-12 w-12 hidden '>
                     
                     <i class="fa-solid fa-ellipsis-vertical text-4xl text-slate-600 bg-white p-2"></i>
                 </span>
